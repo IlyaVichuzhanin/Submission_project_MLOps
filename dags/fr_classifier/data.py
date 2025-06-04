36a91bd97d23562bcf89443313f8f55e099a1799
ilya vichuzhanin <vichuzhanin.ilya@gmail.com>
2025-06-04 22:34:38 +0500

    последние изменения

--- dags/fr_classifier/data.py
+++ dags/fr_classifier/data.py
@@ -8,46 +8,75 @@ import logging
 
 
 def load_data():
-
-    # os.chdir("./")
-    # subprocess.run(["sudo dvc", "pull"], capture_output=True, text=True)
-
-    logger = logging.getLogger(__name__)
-    
-    # Проверка текущей директории
-    current_dir = os.getcwd()
-    logger.info(f"Current directory: {current_dir}")
-    
     try:
-        # Проверка PATH
-        path = os.environ.get("PATH")
-        logger.info(f"PATH: {path}")
-        
-        # Проверка наличия dvc в PATH
         result = subprocess.run(
-            ["which", "dvc"],
+            [f"/home/airflow/.local/bin/dvc", "pull"],
             capture_output=True,
             text=True,
             check=True
         )
-        logger.info(f"dvc found at: {result.stdout.strip()}")
-        
-        # Выполнение dvc pull
-        result = subprocess.run(
-            ["dvc", "pull"],
-            capture_output=True,
-            text=True,
-            check=True
-        )
-        
-        logger.info("dvc pull completed successfully.")
-        logger.debug(f"Output: {result.stdout}")
-        logger.debug(f"Errors: {result.stderr}")
+        print("stdout:", result.stdout)
+        print("stderr:", result.stderr)
     except subprocess.CalledProcessError as e:
-        logger.error(f"Command failed with error: {e}")
-        logger.error(f"Output: {e.output}")
-        logger.error(f"Errors: {e.stderr}")
+        print("Command failed with error:")
+        print(e.stderr)
         raise
+
+    # logger = logging.getLogger(__name__)
+
+    # try:
+    #     result = subprocess.run(['dvc', '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    #     if result.returncode == 0:
+    #         logger.info("✅ DVC доступен в PATH.")
+    #         print("✅ DVC доступен в PATH.")
+    #         print("Версия DVC:", result.stdout.strip())
+    #     else:
+    #         print("❌ DVC не установлен глобально.")
+    #         logger.info("❌ DVC не установлен глобально.")
+    # except FileNotFoundError:
+    #     print("❌ DVC не найден в PATH.")
+    #     logger.info("❌ DVC не найден в PATH.")
+    
+
+    # logger = logging.getLogger(__name__)
+    
+    # # Проверка текущей директории
+    # current_dir = os.getcwd()
+    # logger.info(f"Current directory: {current_dir}")
+    
+    # try:
+    #     # Проверка PATH
+    #     path = os.environ.get("PATH")
+    #     logger.info(f"PATH: {path}")
+        
+    #     # Проверка наличия dvc в PATH
+    #     result = subprocess.run(
+    #         ["which", "dvc"],
+    #         capture_output=True,
+    #         text=True,
+    #         check=True
+    #     )
+    #     logger.info(f"dvc found at: {result.stdout.strip()}")
+        
+    #     # Выполнение dvc pull
+    #     result = subprocess.run(
+    #         ["dvc", "pull"],
+    #         capture_output=True,
+    #         text=True,
+    #         check=True
+    #     )
+        
+    #     logger.info("dvc pull completed successfully.")
+    #     logger.debug(f"Output: {result.stdout}")
+    #     logger.debug(f"Errors: {result.stderr}")
+    # except subprocess.CalledProcessError as e:
+    #     logger.error(f"Command failed with error: {e}")
+    #     logger.error(f"Output: {e.output}")
+    #     logger.error(f"Errors: {e.stderr}")
+    #     raise
+
+
+    
 
 
 
